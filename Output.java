d90345dadd0f5210e35a371a0d1c5a69f3c0aed4
Isuri <tmsisuri.rathnayake@gmail.com>
2024-03-27 17:51:19 +0530

    java output

---
+++ Output.java
@@ -0,0 +1,13 @@
+public class Output {
+    public static void main(String[] args) {
+        //output text
+        System.out.println("Hello World!");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+
+        //output numbers
+        System.out.println(300);
+        System.out.println(3+45);
+        System.out.println(5*6);
+    } 
+}
