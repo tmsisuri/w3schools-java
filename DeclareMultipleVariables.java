7cd5fc24d2206e1cb9801f68b010cf759e2aa3c0
Isuri <tmsisuri.rathnayake@gmail.com>
2024-03-27 20:34:55 +0530

    declare multiple variables in java

---
+++ DeclareMultipleVariables.java
@@ -0,0 +1,12 @@
+public class DeclareMultipleVariables {
+    public static void main(String[] args) {
+        //declare many variables
+        int x = 5, y = 6, z = 50;
+        System.out.println(x + y + z);
+
+        //one value to multiple variables
+        int a, b, c;
+        a = b = c = 50;
+        System.out.println(a + b + c);
+    }
+}
