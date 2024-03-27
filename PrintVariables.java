0346cf8063899536f39ae0af9ebbe7d89f8c3525
Isuri <tmsisuri.rathnayake@gmail.com>
2024-03-27 20:32:45 +0530

    java print variables

---
+++ PrintVariables.java
@@ -0,0 +1,15 @@
+public class PrintVariables {
+    public static void main(String[] args) {
+        String name = "john";
+        System.out.println("Hello " + name);
+
+        String firstName = "John";
+        String lastName = "Doe";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+
+        int x = 4;
+        int y = 8;
+        System.out.println(x + y);
+    }
+}
