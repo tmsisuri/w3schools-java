39a6646ef0427fddb2b540cbb2a3d0b4bfbd40a5
Isuri <tmsisuri.rathnayake@gmail.com>
2024-03-28 07:25:56 +0530

    boolean data type in java

---
+++ Booleans.java
@@ -0,0 +1,10 @@
+public class Booleans {
+    public static void main(String[] args) {
+        //Java has a boolean data type, which can only take the values true or false
+        //Boolean values are mostly used for conditional testing
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun); //output true
+        System.out.println(isFishTasty); //output false
+    }
+}
