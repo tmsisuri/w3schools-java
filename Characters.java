157391e886d03024e7f5b68a731d5ae4740fb761
Isuri <tmsisuri.rathnayake@gmail.com>
2024-03-29 16:58:50 +0530

    char and string in java

---
+++ Characters.java
@@ -0,0 +1,22 @@
+public class Characters {
+    public static void main(String[] args) {
+        // Characters
+        // The char data type is used to store a single character. The character must be surrounded by single quotes, like 'A' or 'c'
+        char c = 'k';
+        System.out.println(c);
+
+        // you can also use ASCII values to display certain characters
+        char c1 = 65, c2 = 66, c3 = 67;
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+
+        // Strings
+        // The String data type is used to store a sequence of characters (text). String values must be surrounded by double quotes
+        // A String in Java is actually a non-primitive data type, because it refers to an object. 
+        // The String object has methods that are used to perform certain operations on strings
+        String greeting = "Hello World";
+        System.out.println(greeting);
+
+    }
+}
