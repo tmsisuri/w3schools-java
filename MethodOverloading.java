87d1883bebb87e030f95ae1a196a88829be166c7
Isuri <tmsisuri.rathnayake@gmail.com>
2024-04-22 12:18:47 +0530

    Method overloading in java

---
+++ MethodOverloading.java
@@ -0,0 +1,21 @@
+public class MethodOverloading {
+
+    //With method overloading, multiple methods can have the same name with different parameters
+    //Multiple methods can have the same name as long as the number and/or type of parameters are different
+    
+    static int plusMethod(int x, int y) {
+        return x + y;
+    }
+      
+    static double plusMethod(double x, double y) {
+        return x + y;
+    }
+    public static void main(String[] args) {
+
+        int myNum1 = plusMethod(8, 5);
+        double myNum2 = plusMethod(4.3, 6.26);
+        System.out.println("int: " + myNum1);
+        System.out.println("double: " + myNum2);
+
+    }
+}
