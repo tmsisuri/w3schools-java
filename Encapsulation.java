047f558c55fd37e4d7449c5a1ef902c942e2eebe
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2024-05-21 21:22:58 +0530

    add comments

--- Encapsulation.java
+++ Encapsulation.java
@@ -1,12 +1,18 @@
 class Person {
+    //The meaning of Encapsulation, is to make sure that "sensitive" data is hidden from users. To achieve this, you must
+    //declare class variables/attributes as private
+    //provide public get and set methods to access and update the value of a private variable
     private String name;
 
+    //The get method returns the variable value, and the set method sets the value
+    //Syntax for both is that they start with either get or set, followed by the name of the variable, with the first letter in upper case
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    //The this keyword is used to refer to the current object
+    public void setName(String newName) {
+        this.name = newName;
     }
     
 }
