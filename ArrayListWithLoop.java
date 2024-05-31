85bd40825e5c4cc22f85869ee8348b220296657b
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2024-05-31 21:54:50 +0530

    ArrayList with loop in java

---
+++ ArrayListWithLoop.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class ArrayListWithLoop {
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        for(int i=0 ; i < cars.size() ; i++) {
+            System.out.println(cars.get(i));
+        }
+
+        //for-each loop
+        for(String j : cars) {
+            System.out.println(j);
+        }
+    }
+}
