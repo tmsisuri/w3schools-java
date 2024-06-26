fa03d34c8c483ab7e249579aca52c6ceab5c8b9c
isuri rathnayake <tmsisuri.rathnayake@gmail.com>
2024-06-26 22:37:23 +0530

    Count Number of Words in a String

---
+++ CountWords.java
@@ -0,0 +1,8 @@
+public class CountWords {
+    public static void main(String[] args) {
+        String words = "One Two Three Four";
+        //split() method splits a string into an array of substrings ["One", "Two", "Three", "Four"]
+        int countWords = words.split("\\s").length;
+        System.out.println(countWords);
+    }
+}
